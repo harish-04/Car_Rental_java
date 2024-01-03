df1ba3215c64112a3f35baad4eb60fae86dfa9b2
'harish' <'harishvithanla04@gmail.com'>
2024-01-03 15:51:14 +0530

    java project for car rental system

---
+++ Car_Rental_System/util/DBConnection.java
@@ -0,0 +1,16 @@
+package util;
+
+import java.sql.*;
+
+
+public class DBConnection {
+	
+	static Connection connection = null;
+	
+    public static Connection getConnection() {
+    	String propstr=DBPropertyUtil.getPropertyString("db.properties");
+		connection=DBConnUtil.getConnection(propstr);
+		return connection;
+    }
+
+}
